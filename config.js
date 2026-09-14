50a09dca79a6fc28475ff350a95cfb41c1c03b1f
cordeliawnoble <cordelia.w.noble@gmail.com>
2026-09-14 15:35:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SONATE_DB = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ilhfzulwuuxyidhityoo.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsaGZ6dWx3dXV4eWlkaGl0eW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4NDA5NTgsImV4cCI6MjEwMjQxNjk1OH0.nux1ehaes_1rPSQhaYJ3hdCcipYn3B5PQMokUzSuBl0",
   language: "fr-FR",
   lists: {
     storyTypes: ["Concept d’œuvre","Intrigue","Scène","Personnage","Relation","Lore","Dialogue / citation","Image / ambiance","Autre"],
