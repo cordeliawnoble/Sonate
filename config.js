e5a32ff6abb3679b146e494f1969f621c90c1e95
cordeliawnoble <cordelia.w.noble@gmail.com>
2026-09-14 15:17:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SONATE_DB = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://ilhfzulwuuxyidhityoo.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsaGZ6dWx3dXV4eWlkaGl0eW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4NDA5NTgsImV4cCI6MjEwMjQxNjk1OH0.nux1ehaes_1rPSQhaYJ3hdCcipYn3B5PQMokUzSuBl0"
 };
