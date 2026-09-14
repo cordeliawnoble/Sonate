168704c8e8145695bfceadb5880cc327dc8c54a7
cordeliawnoble <cordelia.w.noble@gmail.com>
2026-09-14 14:33:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.SONATE_CONFIG={endpoint:"",language:"fr-FR"};
+window.SONATE_CONFIG={endpoint:"https://script.google.com/macros/s/AKfycbyHp3AdQP3-0nqabPLQZLSOmrU9TR9XbdHbOiIGrmslHeM5kkQ-awQnkwE5rPq5ekCa_A/exec",language:"fr-FR"};
